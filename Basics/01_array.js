155d9d4925b8bece22048abde50654d2533a0523
shettyharshith33 <harshithshetty033@gmail.com>
2026-07-13 19:57:21 +0530

    learn array part 1

--- Basics/01_array.js
+++ Basics/01_array.js
@@ -1,3 +1,28 @@
-const myArr = [1,2,3,4,5,6,true,"Harshith"];
+//Arrays
+const myArr = [1,2,3,4,5,6];
+const fruits = new Array("Apple","Banana");
 
 console.log(myArr[0])
+
+
+const myArr2 = myArr;
+
+console.log(myArr[0]);
+
+myArr2[0] = 10;
+
+console.log(myArr[0]);
+console.log(myArr2[0]);
+
+console.log(myArr)
+
+
+
+myArr.push(7);
+myArr.push(8);
+myArr.unshift(100);
+console.log(myArr);
+myArr.shift()
+myArr.pop();
+
+console.log(myArr);
